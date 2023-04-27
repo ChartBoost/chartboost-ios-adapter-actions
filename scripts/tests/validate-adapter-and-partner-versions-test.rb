52e3a6ede61e866525624f3a45340bd89501148d
Daniel Barros <daniel.barros@chartboost.com>
2023-04-27 11:39:13 -0700

    Reorganized script files

--- scripts/tests/validate-adapter-and-partner-versions-test.rb
+++ scripts/tests/validate-adapter-and-partner-versions-test.rb
@@ -2,12 +2,12 @@
 
 require_relative 'test-common'
 
-test('../validate-adapter-and-partner-versions.rb', ['4.3.2.0.0', '3.2.0'], 0)
-test('../validate-adapter-and-partner-versions.rb', ['4.3.2.0.3', '3.2.0'], 0)
-test('../validate-adapter-and-partner-versions.rb', ['4.3.2.0.3', '~> 3.2.0'], 0)
-test('../validate-adapter-and-partner-versions.rb', ['4.3.2.0.3.2', '3.2.0.3'], 0)
-test('../validate-adapter-and-partner-versions.rb', ['4.3.2.0.3.2', '~> 3.2.0.3'], 0)
-test('../validate-adapter-and-partner-versions.rb', ['4.3.2.0.0', '3.1.0'], 1)
-test('../validate-adapter-and-partner-versions.rb', ['4.1.2.0.0', '3.1.0'], 1)
-test('../validate-adapter-and-partner-versions.rb', ['4.3.2.0.0', '3.2.1'], 1)
-test('../validate-adapter-and-partner-versions.rb', ['4.3.2.0.0', '~> 3.2.1'], 1)
+test('../adapters/validate-adapter-and-partner-versions.rb', ['4.3.2.0.0', '3.2.0'], 0)
+test('../adapters/validate-adapter-and-partner-versions.rb', ['4.3.2.0.3', '3.2.0'], 0)
+test('../adapters/validate-adapter-and-partner-versions.rb', ['4.3.2.0.3', '~> 3.2.0'], 0)
+test('../adapters/validate-adapter-and-partner-versions.rb', ['4.3.2.0.3.2', '3.2.0.3'], 0)
+test('../adapters/validate-adapter-and-partner-versions.rb', ['4.3.2.0.3.2', '~> 3.2.0.3'], 0)
+test('../adapters/validate-adapter-and-partner-versions.rb', ['4.3.2.0.0', '3.1.0'], 1)
+test('../adapters/validate-adapter-and-partner-versions.rb', ['4.1.2.0.0', '3.1.0'], 1)
+test('../adapters/validate-adapter-and-partner-versions.rb', ['4.3.2.0.0', '3.2.1'], 1)
+test('../adapters/validate-adapter-and-partner-versions.rb', ['4.3.2.0.0', '~> 3.2.1'], 1)
