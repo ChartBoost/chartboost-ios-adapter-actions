678afe5259ec3a6f5f371e1d41b66418fba35b28
Daniel Barros <daniel.barros@chartboost.com>
2023-11-07 14:07:25 -0800

    Run smoke tests before release

--- scripts/adapters/validate-changelog.rb
+++ scripts/adapters/validate-changelog.rb
@@ -1,12 +1,9 @@
-# Runs validations before releasing a new version of an adapter.
+# Validates that a changelog entry exists for the adapter version.
 
 require_relative 'common'
 
-# Parse the version string from the arguments
-abort "Missing argument. Requires: version string." unless ARGV.count == 1
-version = ARGV[0]
-
 # Validate that the changelog contains an entry for the version to be released.
 changelog = read_changelog
+version = podspec_version
 regex = /^### #{version}$/m
 abort "Release validation failed: entry for #{version} not found in the CHANGELOG." unless changelog.match?(regex)
