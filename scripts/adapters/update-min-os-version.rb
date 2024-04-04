25889ac80ec67a38a01437e1d2a7262d67851eea
Daniel Barros <daniel.barros@chartboost.com>
2024-04-04 12:45:55 -0700

    Add trunk repo explicitly

--- scripts/adapters/update-min-os-version.rb
+++ scripts/adapters/update-min-os-version.rb
@@ -6,6 +6,12 @@ require 'open3'
 
 # Function to obtain the min OS version info from the partner podspec
 def min_os_version(pod_name, pod_version)
+  # Attempt to add the trunk repo explicitly
+  stdout_str, stderr_str, status = Open3.capture3('pod', 'repo', 'add', 'trunk', 'https://github.com/CocoaPods/Specs.git')
+  unless status.success?
+    puts "`pod repo add trunk` error: #{stdout_str} #{stderr_str}"
+  end
+
   # Update the pod repos to ensure the latest info is available
   stdout_str, stderr_str, status = Open3.capture3('pod', 'setup')
   unless status.success?
