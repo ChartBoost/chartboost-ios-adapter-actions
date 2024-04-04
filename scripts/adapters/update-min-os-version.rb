1bfefeafabbd328507fe360f34f90e5d0d1899c1
Daniel Barros <daniel.barros@chartboost.com>
2024-04-04 11:53:37 -0700

    WIP

--- scripts/adapters/update-min-os-version.rb
+++ scripts/adapters/update-min-os-version.rb
@@ -6,8 +6,9 @@ require 'json'
 # Function to obtain the min OS version info from the partner podspec
 def min_os_version(pod_name, pod_version)
   # Update the pod repos to ensure the latest info is available
-  `pod repo update`
-  
+  result = `pod repo update`
+  abort("#{result}")
+
   # Use the `pod spec cat` command to get the podspec as JSON
   podspec_json = `pod spec cat #{pod_name} --version=#{pod_version}`
   begin
