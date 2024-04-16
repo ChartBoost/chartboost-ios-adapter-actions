b1f2da4fa2239af951d531e4c4eec862da0e9051
Kendall Rogers <kendall.rogers@chartboost.com>
2024-04-16 11:37:18 -0700

    Use Gem::Version

--- scripts/adapters/update-min-os-version.rb
+++ scripts/adapters/update-min-os-version.rb
@@ -5,13 +5,7 @@ require 'json'
 require 'open3'
 
 def sdk_min_os_version
-  platform = ENV['CHARTBOOST_PLATFORM']
-  if platform == 'Core'
-    return '11.0'
-  else
-    # fallback to 'Mediation'
-    return '13.0'
-  end
+  return Gem::Version.new('13.0')
 end
 
 # Function to obtain the min OS version info from the partner podspec
@@ -49,20 +43,22 @@ partner_version = ARGV[0]
 partner_version = partner_version.delete_prefix('~> ')
 
 # Obtain the min OS versions for the current adapter and the desired partner version
-partner_min_os_version = min_os_version(podspec_partner_sdk_name(), partner_version)
-current_adapter_version = podspec_min_os_version()
+# Convert to `Gem::Version`, otherwise the string '2.0' is greater than '11.0'.
+partner_min_os_version = Gem::Version.new(in_os_version(podspec_partner_sdk_name(), partner_version))
+current_adapter_version = Gem::Version.new(podspec_min_os_version())
+# Always choose the max of `sdk_min_os_version` and `partner_min_os_version`, so that we never
+# downgrade below the `sdk_min_os_version`.
+new_min_os_version = [sdk_min_os_version, partner_min_os_version].max
 
 # Keep a list of modified files to output at the end
 modified_files = []
 
 # Update adapter files with the new min OS version if it's greater than the current version.
-# Always choose the max of `sdk_min_os_version` and `partner_min_os_version`, so that we never
-# downgrade below the `sdk_min_os_version`.
-if max(sdk_min_os_version, partner_min_os_version) > current_adapter_version
+if new_min_os_version != current_adapter_version
   # Read the podspec file
   podspec = read_podspec()
   # Replace the min OS version string (capture group 2), keeping everything else the same (capture groups 1 and 3)
-  podspec = podspec.sub(PODSPEC_MIN_OS_VERSION_REGEX, "\\1#{partner_min_os_version}\\3")
+  podspec = podspec.sub(PODSPEC_MIN_OS_VERSION_REGEX, "\\1#{new_min_os_version}\\3")
   # Write the changes
   write_podspec(podspec)
   modified_files.append(podspec_file_path)
@@ -70,7 +66,7 @@ if max(sdk_min_os_version, partner_min_os_version) > current_adapter_version
   # Read the readme file
   readme = read_readme()
   # Replace the min OS version string (capture group 2), keeping everything else the same (capture groups 1 and 3)
-  readme = readme.sub(README_MIN_OS_VERSION_REGEX, "\\1#{partner_min_os_version}\\3")
+  readme = readme.sub(README_MIN_OS_VERSION_REGEX, "\\1#{new_min_os_version}\\3")
   # Write the changes
   write_readme(readme)
   modified_files.append(readme_file_path)
@@ -79,7 +75,7 @@ if max(sdk_min_os_version, partner_min_os_version) > current_adapter_version
   changelog = read_changelog()
   # Add a note to the corresponding entry
   entry_header = changelog_entry_header(podspec_version)
-  changelog = changelog.sub(entry_header, "#{entry_header}\n- The minimum OS version required is now iOS #{partner_min_os_version}.")
+  changelog = changelog.sub(entry_header, "#{entry_header}\n- The minimum OS version required is now iOS #{new_min_os_version}.")
   # Write the changes
   write_changelog(changelog)
   modified_files.append(CHANGELOG_PATH)
